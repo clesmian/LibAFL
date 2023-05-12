c93b3aba1a678f64ece8de516e4df7e2ed9b0a93
Leon Weiß <leon.weiss@rub.de>
2023-05-12 12:28:09 +0200

    Add default value for DATA_MAP_SIZE

--- afl-fuzz-clone/src/main.rs
+++ afl-fuzz-clone/src/main.rs
@@ -3,7 +3,8 @@ use std::path::PathBuf;
 use std::time::Duration;
 use konst::{
     primitive::parse_usize,
-    result::unwrap_ctx
+    result::unwrap_ctx,
+    option::unwrap_or,
 };
 
 use clap::Parser;
@@ -172,7 +173,7 @@ fn main() {
 
     let mut run_client = |state: Option<_>, mut mgr, core_id: CoreId| unsafe {
         const CODE_MAP_SIZE: usize = 1 << 16;
-        const DEFAULT_DATA_MAP_SIZE: usize = unwrap_ctx!(parse_usize(env!("DATA_MAP_SIZE"))); // 1<<17 = 131072
+        const DEFAULT_DATA_MAP_SIZE: usize = unwrap_ctx!(parse_usize(unwrap_or!(option_env!("DATA_MAP_SIZE"), "131072"))); // 1<<17 = 131072
 
         #[cfg(not(feature="variable-data-map-size"))]
             let map_size: usize = CODE_MAP_SIZE + DEFAULT_DATA_MAP_SIZE;
