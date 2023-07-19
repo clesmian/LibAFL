2a94208f24bb1f8e83e51fd3028a9dbab6026b62
Leon Weiß <leon.weiss@rub.de>
2023-07-19 09:25:16 +0200

    Directly use constant numbers instead of string parsing

--- zafl_constants/src/lib.rs
+++ zafl_constants/src/lib.rs
@@ -1,8 +1,10 @@
 use konst::{
     primitive::parse_usize,
-    result::unwrap_ctx,
-    option::unwrap_or,
+    result::unwrap_or as res_unwrap_or,
+    option::unwrap_or as opt_unwrap_or,
 };
 
 pub const CODE_MAP_SIZE: usize = 1 << 16;
-pub const DEFAULT_DATA_MAP_SIZE: usize = unwrap_ctx!(parse_usize(unwrap_or!(option_env!("DATA_MAP_SIZE"), "131072"))); // 1<<17 = 131072
+
+pub const DEFAULT_DATA_MAP_SIZE: usize =
+    res_unwrap_or!(parse_usize(opt_unwrap_or!(option_env!("DATA_MAP_SIZE"), "")), 1 << 17);
