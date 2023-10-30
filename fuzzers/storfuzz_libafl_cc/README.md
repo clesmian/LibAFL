29f0d409c1a8a0de3684e6fa324c106946c1a63f
Leon Weiß <leon.weiss@rub.de>
2023-10-30 13:08:46 +0100

    WIP Clang wrapper including StorFuzz Pass

---
+++ fuzzers/storfuzz_libafl_cc/README.md
@@ -0,0 +1,8 @@
+# Code to wrap clang with included StorFuzz pass
+
+Derived from `forkserver_libafl_cc`
+
+## Usage
+
+You can build the compiler-wrapper by running `cargo make`.  
+This compiles libafl_cc  
---
+++ fuzzers/storfuzz_libafl_cc/src/lib.rs
@@ -0,0 +1,17 @@
+use libafl_bolts::ctor;
+use libafl_targets::{map_shared_memory, start_forkserver};
+
+#[cfg(feature = "auto_init_forkserver")]
+#[ctor]
+fn constructor(){
+    println!("LIBAFL_FORKSERVER_AUTO_INIT");
+    libafl_start_forkserver();
+}
+
+#[no_mangle]
+pub extern "C" fn libafl_start_forkserver() {
+    // Map shared memory region for the edge coverage map
+    map_shared_memory();
+    // Start the forkserver
+    start_forkserver();
+}
