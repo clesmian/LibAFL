clesmian/LibAFL

commit f1b3f7e44ebc390e50635f74681592fc5f6cda9c

Add configure mode to skip instrumentation of configure time binaries

libafl_cc/src/storfuzz-coverage-pass.cc
@@ -220,6 +220,12 @@ PreservedAnalyses StorFuzzCoverage::run(Module &M, ModuleAnalysisManager &MAM) {
 #else
 bool StorFuzzCoverage::runOnModule(Module &M) {
 #endif
+  if (getenv("CONFIGURE_MODE")){
+    fprintf(stderr,
+            "WARNING: CONFIGURE_MODE, not doing anything\n");
+    return true;
+  }
+
   LLVMContext &C = M.getContext();
 
 

commit 4694a91291f559d65dc861403a79a7d0f1effa3e

Fix bug introduced in f1b3f7e44ebc390e50635f74681592fc5f6cda9c

libafl_cc/src/storfuzz-coverage-pass.cc
@@ -224,7 +224,11 @@ bool StorFuzzCoverage::runOnModule(Module &M) {
 #endif
   if (getenv("CONFIGURE_MODE")) {
     fprintf(stderr, "WARNING: CONFIGURE_MODE, not doing anything\n");
+#ifdef USE_NEW_PM
+    return PreservedAnalyses::all();
+#else
     return true;
+#endif
   }
 
   LLVMContext &C = M.getContext();