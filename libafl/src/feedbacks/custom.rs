52c1194415123f9b7d2a9c860cbcc36b39680a93
Leon Weiß <leon.weiss@rub.de>
2024-03-08 10:33:31 +0100

    Allow CustomFeedback is_interesting_func to access the state

--- libafl/src/feedbacks/custom.rs
+++ libafl/src/feedbacks/custom.rs
@@ -7,29 +7,33 @@ use crate::events::EventFirer;
 use crate::executors::ExitKind;
 use crate::feedbacks::Feedback;
 use crate::observers::ObserversTuple;
-use crate::prelude::State;
+use crate::prelude::{HasNamedMetadata, State};
 use alloc::string::{String, ToString};
+use std::marker::PhantomData;
 
 /// The [`CustomFeedback`] takes a closure and reports the result of executing this closure.
 #[derive(Serialize, Deserialize, Clone, Debug)]
-pub struct CustomFeedback<F>
-where F: Fn() -> bool,
+pub struct CustomFeedback<'a, F, S>
+    where
+        S: State + HasNamedMetadata,
+        F: Fn(&mut S) -> bool,
 {
     name: String,
     is_interesting_func: F,
-    last_status: bool
+    last_status: Option<bool>,
+    _state: PhantomData<&'a S>
 }
 
-impl<S, F> Feedback<S> for CustomFeedback<F>
+impl<F, S> Feedback<S> for CustomFeedback<'_, F, S>
     where
-        S: State,
-        F: Fn() -> bool,
+        F: Fn(&mut S) -> bool,
+        S: State + HasNamedMetadata
 {
     #[inline]
     #[allow(clippy::wrong_self_convention)]
     fn is_interesting<EM, OT>(
         &mut self,
-        _state: &mut S,
+        state: &mut S,
         _manager: &mut EM,
         _input: &S::Input,
         _observers: &OT,
@@ -39,33 +43,39 @@ impl<S, F> Feedback<S> for CustomFeedback<F>
             EM: EventFirer<State=S>,
             OT: ObserversTuple<S>,
     {
-        let current_status = (self.is_interesting_func)();
-        if current_status != self.last_status {
+        let current_status = (self.is_interesting_func)(state);
+        if !self.last_status.is_some_and(|last| -> bool {last == current_status}) {
             // Allow for individual filtering of different CustomFeedbacks
             // by supplying its name as target
             log::info!(target: &self.name,"Switching status to {}", current_status);
-            self.last_status = current_status;
+            self.last_status = Some(current_status);
         }
         Ok(current_status)
     }
 }
 
-impl<F> Named for CustomFeedback<F>
-where F: Fn() -> bool,{
+impl<F, S> Named for CustomFeedback<'_, F, S>
+    where
+        F: Fn(&mut S) -> bool,
+        S: State + HasNamedMetadata
+{
     #[inline]
     fn name(&self) -> &str { self.name.as_str() }
 }
 
-impl<F>  CustomFeedback<F>
-where F: Fn() -> bool, {
+impl<F, S>  CustomFeedback<'_, F, S>
+    where
+        F: Fn(&mut S) -> bool,
+        S: State + HasNamedMetadata
+{
     /// Creates a new [`CustomFeedback`] with the given name and closure
     #[must_use]
     pub fn new(name: &'static str, func: F) -> Self {
-        let temp = func();
         Self{
             name: name.to_string(),
             is_interesting_func: func,
-            last_status: temp
+            last_status: None,
+            _state: PhantomData::default()
         }
     }
 }
